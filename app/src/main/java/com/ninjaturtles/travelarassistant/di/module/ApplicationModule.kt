64c1d8034116e4419f03e3291e81de1310b5e565
KoliaHuk <m.hyk@polytech.software>
2021-01-21 10:58:24 +0200

    inject dependencies

--- app/src/main/java/com/ninjaturtles/travelarassistant/di/module/ApplicationModule.kt
+++ app/src/main/java/com/ninjaturtles/travelarassistant/di/module/ApplicationModule.kt
@@ -6,7 +6,10 @@ import com.mapbox.android.core.location.LocationEngineProvider
 import com.mapbox.android.core.location.LocationEngineRequest
 import dagger.Module
 import dagger.Provides
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Named
 import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
 
 @Module
 abstract class ApplicationModule {
@@ -23,6 +26,15 @@ abstract class ApplicationModule {
             .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
             .setMaxWaitTime(MAX_WAIT_TIME)
             .build()
+
+        @Provides
+        @Singleton
+        fun provideResources(context: Context) = context.resources
+
+        @Provides
+        @Singleton
+        @Named("io")
+        fun provideCoroutineContext(): CoroutineContext = Dispatchers.IO
 
         private const val INTERVAL = 1000L
         private const val MAX_WAIT_TIME = 5000L
